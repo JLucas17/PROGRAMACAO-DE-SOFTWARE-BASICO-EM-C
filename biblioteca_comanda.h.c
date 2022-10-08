de11a3bcfe6d92f77cc87689c36257e435831ced
João Lucas <114292881+JLucas17@users.noreply.github.com>
2022-10-08 16:16:15 -0300

    commit

---
+++ biblioteca_comanda.h.c
@@ -0,0 +1,17 @@
+// Função Máximo para calcular o valor total Da Comanda
+
+int max (int vt_comanda, int vt_produto) {
+	
+	int resultado;
+	
+	if( vt_comanda= vt_produto);
+	
+	resultado = vt_produto
+	
+	else 
+	
+	resultado = vt_comanda;
+	
+	return resultado;
+	
+}
